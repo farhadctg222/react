68e30f8734af150feef4ca24fe0bc76c6ccac29b
farhadctg22201~ <farhadctg222@gmail.com>
2023-08-06 23:59:20 +0600

    fist commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ReactDOM } from 'react-dom/client';
+
+
 
 function App() {
+  const nayoks = ['rajjak','rahim','rohim','rafi']
+    
   return (
     <div className="App">
+      {
+        nayoks.map((nayok)=> <Myfunction name={nayok}></Myfunction>)
+      }
+      
+      <h2>hello world</h2>
+      <Myfunction></Myfunction>
+     
+      
+      
+      
       <header className="App-header">
+     
+      
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          
         </p>
         <a
           className="App-link"
@@ -15,11 +34,34 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          <farhad></farhad>
           Learn React
         </a>
       </header>
     </div>
   );
+}
+
+function Myfunction(){
+
+  const [count,setcount] = useState(10)
+  console.log(setcount)
+  const handle = ()=> setcount(count+1)
+  return (
+    <div>
+      <button onClick={handle}>add</button>
+      <h1>add movie {count}</h1>
+      <Movies name={count +10}></Movies>
+
+      <Movies name={count +30}></Movies>
+      <Movies name={count +2}></Movies>
+      <Movies name={count +9}></Movies>
+    </div>
+  )
+}
+function Movies(props){
+  return <h1>hello world {props.name} </h1>
+
 }
 
 export default App;
